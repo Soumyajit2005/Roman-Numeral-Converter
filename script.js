8aa24ee2e62bbc4ff2c04252faea85fec5942919
Soumyajit Paria <soumyajitparia2005@gmail.com>
2024-03-23 20:20:30 +0530

    Completed Proect

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
-const number = document.getElementById('number');
-const convertButton = document.getElementById('convert-btn');
-const output = document.getElementById('output');
+const number = document.getElementById("number");
+const convertButton = document.getElementById("convert-btn");
+const output = document.getElementById("output");
 
-convertButton.addEventListener('click', () =>{
-    output.style.display = 'inline-block';
-    if(number.value === ""){
-        output.innerHTML = "Please enter a valid number";
+const romanNumbers = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
+
+convertButton.addEventListener("click", () => {
+  output.style.display = "inline-block";
+  if (number.value === "") {
+    output.innerHTML = "Please enter a valid number";
+  } else {
+    let num = number.value;
+    if (num < 1) {
+      output.innerHTML = "Please Enter a number greater than or equal to 1";
+    } else if (num >= 4000) {
+      output.innerHTML = "Please enter a number less than or equal to 3999";
+    } else {
+      let number = convertNum(romanNumbers, num);
+      output.innerText = number;
     }
-    else{
-        let num = number.value;
-        if( num < 1){
-            output.innerHTML = "Please Enter a number greater than or equal to 1";
-        }
-        else if(num >= 4000){
-            output.innerHTML = "Please enter a number less than or equal to 3999";
-        }
+  }
+});
+
+const convertNum = (romanNums, num) => {
+  let convertedNum = "";
+  const roman = ["M",'CM', "D",'CD', "C",'XC', "L", 'XL', "X", 'IX', "V", 'IV', "I"];
+  for (let i = 0; i < roman.length; i++) {
+    if ((num - romanNums[roman[i]]) >= 0) {
+      convertedNum += roman[i];
+      num -= romanNums[roman[i]];
+      i = -1;
     }
-})
+  }
+  return convertedNum;
+};
